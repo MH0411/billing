81a59b0440e14db6361f89097749011048180792
mike0411 <z.mike0411@gmail.com>
2016-03-23 18:02:31 +0800

    Server Detail class added

---
+++ eKlinik/src/eklinik_bill/ServerDetail.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eklinik_bill;
+
+/**
+ * To monitor host and port easily
+ * @author user
+ */
+public class ServerDetail {
+    
+    // declaration host and port
+    //String host = "biocore-stag.utem.edu.my"; //server stagging
+    //String host = "biocore-devp.utem.edu.my"; // server development
+    private final String host;
+    private final int port;
+    
+    public ServerDetail(){
+        this.host = "10.73.32.200";
+        this.port = 1099;
+    }
+    
+    public String getHost(){
+        return this.host;
+    }
+    
+    public int getPort(){
+        return this.port;
+    }
+}
